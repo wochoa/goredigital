7356aba2766ccdd5645a5b04979ecab17bf63590
unknown <woalvarado1@gmail.com>
2021-08-25 17:55:55 -0500

    decimo

--- resources/views/livewire/fondo.blade.php
+++ resources/views/livewire/fondo.blade.php
@@ -3,9 +3,9 @@
     
         <a wire:click="cambiofondo({{ Auth::user()->id }},'{{ Request::url() }}')" class="nav-link"  role="button" >
           @if(Auth::user()->darkmode)
-          <i class="fas fa-toggle-on"></i> 
+          <i class="fas fa-adjust"></i> 
           @else
-          <i class="fas fa-toggle-off"></i>
+          <i class="far fa-moon"></i>
           @endif
         </a>
 
