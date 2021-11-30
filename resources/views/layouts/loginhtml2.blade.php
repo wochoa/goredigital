7cc48df0cf1b680496e8aa070d6f6102c53ede6f
unknown <woalvarado1@gmail.com>
2021-11-30 16:41:18 -0500

    31

--- resources/views/layouts/loginhtml2.blade.php
+++ resources/views/layouts/loginhtml2.blade.php
@@ -6,9 +6,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
+    <meta name="description" content="goredigital, Gobierno Digital,gobierno digital,gobierno regional hu8anuco">
+    <meta name="keywords" content="goredigital,Gobierno Digital">
+    <meta name="author" content="wochoa">
     <title>GoreDigital</title>
     <link rel="icon" type="image/png" href="{{ asset('dist/img/favicon.png') }}">
     {{-- <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico"> --}}
